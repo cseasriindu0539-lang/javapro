7de5451193e7aeb2eca0fb1ed0f2aea035096300
reader45 <kotlasaikiran742@gmail.com>
2025-10-17 11:43:19 +0530

    this is now ok

--- program.java
+++ program.java
@@ -1,8 +1,9 @@
 public class GFG{
     public static void main(String[] args){
         
-        int m = 9, n = 5;
-        System.out.println("Before swapping: m = " + m + ", n = " + n);
+       
+     int m = 9, n = 5; //this is my changes in the second time
+        System.out.println("Before swapping: m = " + m + ", n = " + n);//after thiswe look for the chnges commitedor not
 
         int temp = m;
         m = n;
